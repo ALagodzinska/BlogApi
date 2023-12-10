a0c841fda3c7939db5f9aa491c4d67bcfc94943a
Anastasija Lagodzinska <a.lagodzinska@outlook.com>
2023-12-10 13:06:09 -0800

    Add project files.

---
+++ BlogApi/BlogPost.cs
@@ -0,0 +1,17 @@
+﻿namespace BlogApi
+{
+    public class BlogPost
+    {
+        public int BlogPostId { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? Content { get; set; }
+
+        public string? Image { get; set; }
+
+        public string? User { get; set; }
+    }
+}
---
+++ BlogApi/BloggingContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlogApi
+{
+    public class BloggingContext : DbContext
+    {
+        public BloggingContext(DbContextOptions<BloggingContext> options)
+       : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=BlogWebsiteDb");
+    }
+}
